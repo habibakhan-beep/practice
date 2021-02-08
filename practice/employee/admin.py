75af06e7b923ceceb5383ae80d2ddb459e84ef6e
DESKTOP-L3C5HUL\Admission <habiba.khan@siparadigm.com>
2021-02-08 16:09:03 +0500

    Project structure

---
+++ practice/employee/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from .models import Employee, Department
+from simple_history.admin import SimpleHistoryAdmin
+# Register your models here.
+
+
+class EmployeeAdmin(SimpleHistoryAdmin):
+    pass
+
+
+class DepartmentAdmin(SimpleHistoryAdmin):
+    pass
+
+
+admin.site.register(Employee, EmployeeAdmin)
+admin.site.register(Department, DepartmentAdmin)
+
